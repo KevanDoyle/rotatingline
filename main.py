9eb5da5e8a3bf5fba7e86878a5709184ac44fd46
Kevan Doyle <kevan.doyle@gmail.com>
2024-06-21 10:23:00 -0600

    Trying out on_button_pressed commands as well as arrays of strings in python

--- main.py
+++ main.py
@@ -1,68 +1,58 @@
 dly = 100
+blinks = [
+"""
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
+    """,
+    """
+    . . . . #
+    . . . # .
+    . . # . .
+    . # . . .
+    # . . . .
+    """,
+    """
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    """,
+    """
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    """
+]
+def on_button_pressed_a():
+'''
+    basic.show_leds(blinks[0])
+    basic.pause(dly)
+    basic.show_leds(blinks[1])
+    basic.pause(dly)
+'''
+    led.toggle(0, 0)
+    led.toggle(0,5)
+    led.toggle(5, 0)
+    led.toggle(5, 0)
+    basic.pause(dly)
+def on_button_pressed_b():
+    basic.show_leds(blinks[2])
+    basic.pause(dly)
+    basic.show_leds(blinks[3])
+    basic.pause(dly)
+def just_blink():
+    led.toggle(2, 2)
+    basic.pause(dly)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    basic.show_leds("""
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        . . . . #
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . # . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . # .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . . # .
-        . . . . .
-        . . # . .
-        . . . . .
-        . # . . .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . . . .
-        . . . . #
-        . . # . .
-        # . . . .
-        . . . . .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(dly)
-    basic.show_leds("""
-        . . . . .
-        # . . . .
-        . . # . .
-        . . . . #
-        . . . . .
-        """)
-    basic.pause(dly)
+    just_blink()
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,67 +1,48 @@
 let dly = 100
+let blinks = [`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
+    `, `
+    . . . . #
+    . . . # .
+    . . # . .
+    . # . . .
+    # . . . .
+    `, `
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    `, `
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `]
+function just_blink() {
+    led.toggle(2, 2)
+    basic.pause(dly)
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    //     basic.show_leds(blinks[0])
+    //     basic.pause(dly)
+    //     basic.show_leds(blinks[1])
+    //     basic.pause(dly)
+    led.toggle(2, 2)
+    basic.pause(dly)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showLeds(blinks[2])
+    basic.pause(dly)
+    basic.showLeds(blinks[3])
+    basic.pause(dly)
+})
 basic.forever(function on_forever() {
-    basic.showLeds(`
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        . . . . #
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . # .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . . # .
-        . . . . .
-        . . # . .
-        . . . . .
-        . # . . .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . # . .
-        # . . . .
-        . . . . .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(dly)
-    basic.showLeds(`
-        . . . . .
-        # . . . .
-        . . # . .
-        . . . . #
-        . . . . .
-        `)
-    basic.pause(dly)
+    just_blink()
 })
